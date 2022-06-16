72fe864b9619b1f3c01644470ea602d5968d9c15
Gianluca Filippone <filgianluca93@gmail.com>
2022-06-16 16:07:47 +0200

    First stub service implementation

---
+++ services/payment/src/main/java/it/univaq/sosygroup/ticketingsystem/prototype/payment/business/PaymentService.java
@@ -0,0 +1,11 @@
+package it.univaq.sosygroup.ticketingsystem.prototype.payment.business;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public void pay(String instanceId) {
+        return;
+    }
+}
