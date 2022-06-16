772b6ea1d6dac22324cccbf3cfbf03b212cc3471
Gianluca Filippone <filgianluca93@gmail.com>
2022-06-16 18:54:07 +0200

    Added stub for CD with client-side load balancer

--- services/payment/src/main/java/it/univaq/sosygroup/ticketingsystem/prototype/payment/business/PaymentService.java
+++ services/payment/src/main/java/it/univaq/sosygroup/ticketingsystem/prototype/payment/business/PaymentService.java
@@ -2,10 +2,21 @@ package it.univaq.sosygroup.ticketingsystem.prototype.payment.business;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 public class PaymentService {
 
+    static int instanceNumber;
+
+    static {
+        instanceNumber = ThreadLocalRandom.current().nextInt(0, 1000);
+        System.out.println("Instantiated PaymentService instance #" + instanceNumber);
+    }
+
     public void pay(String instanceId) {
-        return;
+
+        System.out.println("Called instance #" + instanceNumber);
     }
 }
