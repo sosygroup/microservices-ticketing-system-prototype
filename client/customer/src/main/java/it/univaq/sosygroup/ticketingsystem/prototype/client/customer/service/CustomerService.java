18497dc00abc9e6661fb511e12f6fb2853258f9c
Gianluca Filippone <filgianluca93@gmail.com>
2022-06-23 19:09:31 +0200

    Added customer skeleton

---
+++ client/customer/src/main/java/it/univaq/sosygroup/ticketingsystem/prototype/client/customer/service/CustomerService.java
@@ -0,0 +1,9 @@
+package it.univaq.sosygroup.ticketingsystem.prototype.client.customer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    // TODO insert here methods to simulate client interaction
+}
